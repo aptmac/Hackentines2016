3ae1d3bc630a17b2d6e09cc18e8f7800d4cb2602
Ryan Scovill <ryans797@gmail.com>
2016-02-12 13:49:28 -0800

    Difficulty Activity
    Added seeker bar for activity.  Able to set difficulty

--- app/src/main/java/com/example/norifurikake/hackenties2016/Difficulty.java
+++ app/src/main/java/com/example/norifurikake/hackenties2016/Difficulty.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class Difficulty extends ActionBarActivity {
+
+    private int _difficulty = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +17,23 @@ public class Difficulty extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    public void toGame(View view){
+    public void toGame(View view) {
         Intent intent = new Intent(this, Game.class);
+        intent.putExtra("difficulty", this._difficulty);
         startActivity(intent);
+    }
+    private void setDifficulty(int difficulty){
+        SeekBar seekBar = (SeekBar) findViewById(R.id.difficultyBar);
+        seekBar.setProgress(difficulty);
+        this._difficulty = difficulty;
+    }
+    public void setDifficultyEasy(View view){
+        setDifficulty(0);
+    }
+    public void setDifficultyMedium(View view){
+        setDifficulty(5);
+    }
+    public void setDifficultyHard(View view){
+        setDifficulty(10);
     }
 }
