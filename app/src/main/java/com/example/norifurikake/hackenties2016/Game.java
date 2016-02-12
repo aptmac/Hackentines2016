701fa7a7002f7dc7f4ea63ae7bdc9def10ea74ea
apmacdon <apmacdon@sfu.ca>
2016-02-12 15:10:03 -0800

    Added Increase/Decrease functionality to Game

--- app/src/main/java/com/example/norifurikake/hackenties2016/Game.java
+++ app/src/main/java/com/example/norifurikake/hackenties2016/Game.java
@@ -3,6 +3,7 @@ package com.example.norifurikake.hackenties2016;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 
@@ -22,36 +23,124 @@ public class Game extends ActionBarActivity {
 
     }
 
-
-    public void IncreaseCake(){
+    /**
+     * Increase and Decrease the quantity of Cake
+     */
+    public void IncreaseCake(View v){
         numCake++;
-        Log.w("test", String.valueOf(numCake));
+        Log.d("Increase: ", String.valueOf(numCake));
+        TextView textView = (TextView) findViewById(R.id.txtCake);
+        textView.setText(String.valueOf(numCake));
     }
-    public void DecreaseCake(){
-        numCake--;
+    public void DecreaseCake(View v){
+        if (numCake == 0)
+        {
+            //do nothing
+            Log.d("Error:", "Cannot have negative value.");
+        }
+        else
+        {
+            numCake--;
+            Log.d("Decrease: ", String.valueOf(numCake));
+            TextView textView = (TextView) findViewById(R.id.txtCake);
+            textView.setText(String.valueOf(numCake));
+        }
     }
-    public void IncreaseChocolate(){
+
+    /**
+     * Increase and Decrease the quantity of Chocolate
+     */
+    public void IncreaseChocolate(View v){
         numChocolate++;
+        Log.d("Increase: ", String.valueOf(numChocolate));
+        TextView textView = (TextView) findViewById(R.id.txtChocolate);
+        textView.setText(String.valueOf(numChocolate));
     }
-    public void DecreaseChocolate(){
-        numChocolate--;
+    public void DecreaseChocolate(View v){
+        if (numChocolate == 0)
+        {
+            //do nothing
+            Log.d("Error:", "Cannot have negative value.");
+        }
+        else
+        {
+            numChocolate--;
+            Log.d("Decrease: ", String.valueOf(numChocolate));
+            TextView textView = (TextView) findViewById(R.id.txtChocolate);
+            textView.setText(String.valueOf(numChocolate));
+        }
     }
-    public void IncreaseWine(){
+
+    /**
+     * Increase and Decrease the quantity of Wine
+     */
+    public void IncreaseWine(View v){
         numWine++;
+        Log.d("Increase: ", String.valueOf(numWine));
+        TextView textView = (TextView) findViewById(R.id.txtWine);
+        textView.setText(String.valueOf(numWine));
     }
-    public void DecreaseWine(){
-        numWine--;
+    public void DecreaseWine(View v){
+        if (numWine == 0)
+        {
+            //do nothing
+            Log.d("Error:", "Cannot have negative value.");
+        }
+        else
+        {
+            numWine--;
+            Log.d("Decrease: ", String.valueOf(numWine));
+            TextView textView = (TextView) findViewById(R.id.txtWine);
+            textView.setText(String.valueOf(numWine));
+        }
     }
-    public void IncreaseRose(){
-        numRose++;
-    }
-    public void DecreaseRose(){
-        numRose--;
-    }
-    public void IncreaseCat(){
+
+    /**
+     * Increase and Decrease the quantity of Cat
+     */
+    public void IncreaseCat(View v){
         numCat++;
+        Log.d("Increase: ", String.valueOf(numCat));
+        TextView textView = (TextView) findViewById(R.id.txtCat);
+        textView.setText(String.valueOf(numCat));
     }
-    public void DecreaseCat(){
-        numCat--;
+    public void DecreaseCat(View v){
+        if (numCat == 0)
+        {
+            //do nothing
+            Log.d("Error:", "Cannot have negative value.");
+        }
+        else
+        {
+            numCat--;
+            Log.d("Decrease: ", String.valueOf(numCat));
+            TextView textView = (TextView) findViewById(R.id.txtCat);
+            textView.setText(String.valueOf(numCat));
+        }
     }
+
+    /**
+     * Increase and Decrease the quantity of Rose
+     */
+    public void IncreaseRose(View v){
+        numRose++;
+        Log.d("Increase: ", String.valueOf(numRose));
+        TextView textView = (TextView) findViewById(R.id.txtRose);
+        textView.setText(String.valueOf(numRose));
+    }
+    public void DecreaseRose(View v){
+        if (numRose == 0)
+        {
+            //do nothing
+            Log.d("Error:", "Cannot have negative value.");
+        }
+        else
+        {
+            numRose--;
+            Log.d("Decrease: ", String.valueOf(numRose));
+            TextView textView = (TextView) findViewById(R.id.txtRose);
+            textView.setText(String.valueOf(numRose));
+        }
+    }
+
 }
