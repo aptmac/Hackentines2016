3f4b76517f878b4376d55e7dc02ac47e73d5c657
apmacdon <apmacdon@sfu.ca>
2016-02-12 14:16:08 -0800

    Synced Difficulty

--- app/src/main/java/com/example/norifurikake/hackenties2016/Difficulty.java
+++ app/src/main/java/com/example/norifurikake/hackenties2016/Difficulty.java
@@ -4,18 +4,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class Difficulty extends ActionBarActivity {
+    SeekBar seekBar;
+    private int _difficulty = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_difficulty);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        seekBar = (SeekBar) findViewById(R.id.difficultyBar);
     }
 
-    public void toGame(View view){
+    public void toGame(View view) {
+        this._difficulty = seekBar.getProgress();
         Intent intent = new Intent(this, Game.class);
+        intent.putExtra("difficulty", this._difficulty);
         startActivity(intent);
+    }
+    private void setDifficulty(int difficulty){
+        seekBar.setProgress(difficulty);
+        this._difficulty = difficulty;
+    }
+    public void setDifficultyEasy(View view){
+        setDifficulty(0);
+    }
+    public void setDifficultyMedium(View view){
+        setDifficulty(5);
+    }
+    public void setDifficultyHard(View view){
+        setDifficulty(10);
     }
 }
