4c4c0fd486dbb289b215d82fec54e62584b9cbcc
apmacdon <apmacdon@sfu.ca>
2016-02-12 14:11:29 -0800

    Game Activity
    Added Buttons and Icons to Game Activity

--- app/src/main/java/com/example/norifurikake/hackenties2016/Game.java
+++ app/src/main/java/com/example/norifurikake/hackenties2016/Game.java
@@ -2,14 +2,56 @@ package com.example.norifurikake.hackenties2016;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.TextView;
+
 
 public class Game extends ActionBarActivity {
+
+    public int numCake = 0;
+    public int numChocolate = 0;
+    public int numWine = 0;
+    public int numRose = 0;
+    public int numCat = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
+
+    public void IncreaseCake(){
+        numCake++;
+        Log.w("test", String.valueOf(numCake));
+    }
+    public void DecreaseCake(){
+        numCake--;
+    }
+    public void IncreaseChocolate(){
+        numChocolate++;
+    }
+    public void DecreaseChocolate(){
+        numChocolate--;
+    }
+    public void IncreaseWine(){
+        numWine++;
+    }
+    public void DecreaseWine(){
+        numWine--;
+    }
+    public void IncreaseRose(){
+        numRose++;
+    }
+    public void DecreaseRose(){
+        numRose--;
+    }
+    public void IncreaseCat(){
+        numCat++;
+    }
+    public void DecreaseCat(){
+        numCat--;
+    }
 }
